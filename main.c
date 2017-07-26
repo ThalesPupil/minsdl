d02cb2fc600c03867c2f0d4d394eda7ab61a449d
Miguel Ángel <thales.pupil@gmail.com>
2017-07-26 15:02:15 -0500

    sdl2 test

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
 int main()
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("A Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, SDL_WINDOW_SHOWN);
 	printf("minsdl cambio");
+
+	SDL_Delay(4000);
+
+	SDL_DestroyWindow(window);
 }
